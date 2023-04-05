c71d278aeb0e860a5733c673408741353050a1e3
Robin Klok <klokxx@gmail.com>
2023-04-05 08:50:20 +0200

    gitt commit test exit esc dss
    aa

---
+++ variables/main.py
@@ -0,0 +1,8 @@
+# Do not modify these lines
+__winc_id__ = '7152c06aa3ac4d5f964ca8619ecb7e8f'
+__human_name__ = 'variables'
+
+# Add your code after this line
+var_string = 'yes'
+var_int = 9
+var_bool = True
